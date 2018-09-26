58ff1845434be7bdcb3e77e37ce58d18dc84cd42
hibatallah.aouadni <hibatallah.aouadni@sifast.com>
2018-09-26 11:23:03 +0100

    FO: Organize CSS & fix JS

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,7 @@ jQuery(function($) {
     var anchor = window.location.hash;
     $('.tabs').each(function(){
         var current = null;
-        var id = $(this).attr('id');
+        var id = $(this).attr('id'); console.log(id);
         if(anchor != '' && $(this).find('a[href="' + anchor + '"]').length > 0) {
             current = anchor;
         }else if (Cookies.get('tab' + id) && $(this).find('a[href="' + Cookies.get('tab' + id) + '"]').length > 0) {
@@ -11,8 +11,27 @@ jQuery(function($) {
         }else {
             current = $(this).find('a:first').attr('href');
         }
-        if($('.tabs').parent('#services').length != 0) {
-            $(this).find('a[href="' + current + '"]').parent('.icon').addClass('active');
+        console.log(current);
+        if($('.tabs').parents('#services').length != 0) {
+            $(this).find('a[href="' + current + '"]').addClass('active');
+            switch(current) {
+                case '#bloc1':
+                    $('.bloc-active').css({'transition': 'transform 1s linear', 'transform': 'translateX(0%) rotate(45deg)'});
+                    break;
+                case '#bloc2':
+                    $('.bloc-active').css({'transition': 'transform 1s linear', 'transform': 'translateX(275%) rotate(45deg)'});
+                    break;
+                case '#bloc3':
+                    $('.bloc-active').css({'transition': 'transform 1s linear', 'transform': 'translateX(550%) rotate(45deg)'});
+                    break;
+                case '#bloc4':
+                    $('.bloc-active').css({'transition': 'transform 1s linear', 'transform': 'translateX(820%) rotate(45deg)'});
+                    break;
+                case '#bloc5':
+                    $('.bloc-active').css({'transition': 'transform 1s linear', 'transform': 'translateX(1090%) rotate(45deg)'});
+                    break;
+                default: break;
+            }
         } else {
             $(this).find('a[href="' + current + '"]').addClass('active');
         }
@@ -23,10 +42,10 @@ jQuery(function($) {
             if( link == current) {
                 return false;
             }else {
-                if($('.tabs').parent('#services').length != 0) {
-                    $(this).parent('.icon').siblings().removeClass('active');
-                    $(this).parent('.icon').addClass('active');
-                    switch($(this).parent('.icon').attr('id')) {
+                if($('.tabs').parents('#services').length != 0) {
+                    $(this).siblings().removeClass('active');
+                    $(this).addClass('active');
+                    switch($(this).attr('id')) {
                         case 'pos1':
                             $('.bloc-active').css({'transition': 'transform 1s linear', 'transform': 'translateX(0%) rotate(45deg)'});
                             break;
